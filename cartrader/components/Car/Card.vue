c5a0e9d57bd6953c91d1ec61fef2bd68704b5c41
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-05 14:23:36 -0500

    add CarSidebar, CarCards, and Card components; update city make page layout

---
+++ cartrader/components/Car/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://www.autotrainingcentre.com/wp-content/uploads/2013/07/Lambo2.jpg"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Lambo cc</h1>
+          <p class="text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <h1 class="mt-auto text-xl">87.85$</h1>
+      </div>
+    </div>
+  </div>
+</template>
