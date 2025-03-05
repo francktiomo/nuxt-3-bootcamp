9ead5433b47316b1d193b23ac76450c41cf0d3cb
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-05 14:35:29 -0500

    add car detail components: Hero, Attributes, Description, and Contact

---
+++ cartrader/components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+      cupiditate possimus corporis tenetur omnis aliquid provident debitis,
+      alias totam reiciendis hic nihil modi vel itaque iure cumque delectus
+      temporibus sequi!
+    </p>
+  </div>
+</template>
---
+++ cartrader/components/Car/Detail/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-10">
+    <img
+      src="https://www.autotrainingcentre.com/wp-content/uploads/2013/07/Lambo2.jpg"
+      alt=""
+      class="w-full"
+    />
+    <h1 class="mt-10 text-4xl">Lambo CCC</h1>
+    <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
+      <div class="flex">
+        <p class="mr-2">5 seats</p>
+        <p class="mr-2">|</p>
+        <p class="mr-2">65,444 miles</p>
+      </div>
+      <div>
+        <p class="font-bold text-2xl">$100,000$</p>
+      </div>
+    </div>
+  </div>
+</template>
