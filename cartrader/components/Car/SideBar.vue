c779b6c44a2a4fda660701328c31c720453addf0
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 16:39:03 -0400

    Show price range

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -9,9 +9,23 @@ const modal = ref({
 
 const city = ref("");
 const route = useRoute();
+const router = useRouter();
 const priceRange = ref({
   min: "",
   max: "",
+});
+const priceRangeText = computed(() => {
+  const minPrice = route.query.minPrice;
+  const maxPrice = route.query.maxPrice;
+  if (!minPrice && !maxPrice) {
+    return "Any";
+  } else if (!minPrice && maxPrice) {
+    return `< $${maxPrice}`;
+  } else if (minPrice && !maxPrice) {
+    return `> $${minPrice}`;
+  } else {
+    return `$${minPrice} - $${maxPrice}`;
+  }
 });
 
 const updateModal = (key) => {
@@ -34,6 +48,21 @@ const onChangeLocation = () => {
 const onChangeMake = (make) => {
   updateModal("make");
   navigateTo(`/city/${route.params.city}/car/${make}`);
+};
+
+const onChangePrice = () => {
+  updateModal("price");
+  if (priceRange.value.min && priceRange.value.max) {
+    if (priceRange.value.min > priceRange.value.max) {
+      return;
+    }
+  }
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max,
+    },
+  });
 };
 </script>
 
@@ -83,7 +112,9 @@ const onChangeMake = (make) => {
     <!-- PRICE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">WIP</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+        {{ priceRangeText }}
+      </h3>
       <div
         class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
         v-if="modal.price"
@@ -100,7 +131,10 @@ const onChangeMake = (make) => {
           placeholder="Max"
           v-model="priceRange.max"
         />
-        <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+        <button
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+          @click="onChangePrice"
+        >
           Apply
         </button>
       </div>
