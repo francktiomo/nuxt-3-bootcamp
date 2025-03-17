ede4221f7f04688dede7581bda1000d7cdb8f285
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 13:47:38 -0400

    Enforcing client side rendering to allow persitance with local storage

--- cartrader/components/Car/Cards.vue
+++ cartrader/components/Car/Cards.vue
@@ -1,6 +1,4 @@
 <script setup>
-import Card from './Card.vue';
-
 const { cars } = useCars();
 
 const favorite = useLocalStorage("favorite", {});
@@ -19,12 +17,17 @@ const handleFavorite = (id) => {
 
 <template>
   <div class="w-full">
-    <CarCard
-      v-for="car in cars"
-      :key="car.id"
-      :car="car"
-      @favor="handleFavorite"
-      :favored="car.id in favorite"
-    />
+    <!-- ClientOnly is used to enforce client side rendering on the component it wraps
+    It allows the component to access the LocalStorage
+    Wrapping the component is one way to do it, anoher way is to rename the component file like thi: ComponentName.client.vue -->
+    <ClientOnly>
+      <CarCard
+        v-for="car in cars"
+        :key="car.id"
+        :car="car"
+        @favor="handleFavorite"
+        :favored="car.id in favorite"
+      />
+    </ClientOnly>
   </div>
 </template>
