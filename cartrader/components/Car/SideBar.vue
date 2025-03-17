acdd32ea8c9a656877f27f70fc5bb0fee52fe75d
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 16:03:19 -0400

    Add make selection functionality to sidebar component

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -26,6 +26,11 @@ const onChangeLocation = () => {
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = "";
 };
+
+const onChangeMake = (make) => {
+  updateModal("make");
+  navigateTo(`/city/${route.params.city}/car/${make}`);
+}
 </script>
 
 <template>
@@ -53,11 +58,14 @@ const onChangeLocation = () => {
     <!-- MAKE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{ route.params.make || "Any" }}
+      </h3>
       <div
         class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+        v-if="modal.make"
       >
-        <h4 v-for="make in makes" :key="make" class="w-1/3">{{ make }}</h4>
+        <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">{{ make }}</h4>
       </div>
     </div>
     <!-- MAKE END -->
