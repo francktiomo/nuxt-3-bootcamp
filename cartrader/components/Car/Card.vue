4552d8dd80356076e551ed30f8f2203375477388
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 12:59:32 -0400

    allow user to mark a car as favorite

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -1,22 +1,30 @@
 <script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
 const props = defineProps({
   car: Object,
+});
+
+const favored = useState(`favored-${props.car.id}`, () => {
+  return false;
 });
 </script>
 
 <template>
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)"
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
-    <div class="flex h-full">
-      <NuxtImg
-        :src="car.url"
-        alt=""
-        class="w-[300px] h-full"
-      />
+    <img
+      :src="favored ? heartFilled : heartOutline"
+      class="absolute w-7 right-5 top-2 z-20"
+      @click="favored = !favored"
+    />
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">{{  car.name  }}</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-gray-700">
             {{ car.description }}
           </p>
