695f49dfa82e2fe660c14859b35f114d9971c98d
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 14:17:20 -0400

    Test

--- cartrader/components/Car/SideBar.vue
+++ cartrader/components/Car/SideBar.vue
@@ -31,6 +31,7 @@ const onChangeLocation = () => {
 <template>
   <div class="shadow border w-64 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
+      <!-- LOCATION START -->
       <h3>Location</h3>
       <h3 @click="updateModal('location')" class="text-blue-400 capitalize">
         {{ route.params.city }}
@@ -48,13 +49,23 @@ const onChangeLocation = () => {
         </button>
       </div>
     </div>
+    <!-- LOCATION END -->
+    <!-- MAKE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
       <h3 class="text-blue-400 capitalize">Toyota</h3>
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+      >
+        <h4 v-for="make in makes" :key="make" class="w-1/3">{{ make }}</h4>
+      </div>
     </div>
+    <!-- MAKE END -->
+    <!-- PRICE START -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
       <h3 class="text-blue-400 capitalize">WIP</h3>
     </div>
+    <!-- PRICE END -->
   </div>
 </template>
