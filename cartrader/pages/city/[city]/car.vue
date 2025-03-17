1697dd452f6c5912d73dd3356764946de86e3c82
Franck Tiomo <ef.tiomo@gmail.com>
2025-03-17 12:35:47 -0400

    add NuxtErrorBoundary to handle client-side errors in car page

--- cartrader/pages/city/[city]/car.vue
+++ cartrader/pages/city/[city]/car.vue
@@ -15,8 +15,24 @@ definePageMeta({
 <template>
   <div>
     <div class="mt-32 flex">
-      <CarSideBar />
-      <NuxtPage />
+      <!-- NuxtErrorBoundary: Used to handle client side error -->
+      <NuxtErrorBoundary>
+        <CarSideBar />
+        <NuxtPage />
+        <!-- S'affiche lorsqu'une erreur est catchée par NuxtErrorBoundary -->
+        <template #error="{ error }">
+          <div class="text-center mx-auto flex flex-col">
+            <h1 class="text-5xl text-red-600 mb-4">Something went wrong</h1>
+            <code>{{ error }}</code>
+            <button
+              class="text-white bg-blue-400 px-10 py-3 rounded mt-4"
+              @click="error.value = null"
+            >
+              Go back
+            </button>
+          </div>
+        </template>
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
